bc13074c4986870e1fb5c50138488c50973f592d
paulowensmith68 <paulosmith@tiscali.co.uk>
2016-04-16 22:14:29 +0100

    Add project files.

---
+++ RunPageCommandApp/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main(arg As String())
+
+        If arg.Length < 1 Or arg.Length > 1 Then
+            Console.WriteLine("Arguments Missing")
+            Console.WriteLine("Arguments Are: UrlToRun")
+            Return
+        End If
+
+        Dim webUrl As String = arg(0).ToString
+
+        Dim loadpage As New RunPage
+
+        Try
+            Console.WriteLine(loadpage.getUrl(webUrl))
+        Catch e As Exception
+            Console.WriteLine("-- ERROR --")
+            Console.WriteLine(e.Message)
+            Exit Try
+        End Try
+
+    End Sub
+
+End Module
---
+++ RunPageCommandApp/RunPage.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Net
+Imports System.IO
+Friend Class RunPage
+    Public Shared Function getUrl(URL As String) As String
+        Dim myRequest As HttpWebRequest = DirectCast(WebRequest.Create(URL), HttpWebRequest)
+        myRequest.Method = "GET"
+        Dim myResponse As WebResponse = myRequest.GetResponse()
+        Dim sr As New StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8)
+        Dim result As String = sr.ReadToEnd()
+        sr.Close()
+        myResponse.Close()
+        Return result
+    End Function
+
+End Class
